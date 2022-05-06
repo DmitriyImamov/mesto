af41a051af9b4c34ed9b9ab42eb3abaf1f9db759
Dmitriy Imamov <dmitriyimamov@gmail.com>
2022-05-07 02:14:06 +0300

    end work validation form

--- scripts/index.js
+++ scripts/index.js
@@ -34,68 +34,74 @@ const profileInfoCurenetText = body.querySelector('.profile__info-curenet-text')
 const profileAddButton = body.querySelector('.profile__add-button');
 const popupPlaceProfile = body.querySelector('.popup_place_profile');
 const profilePopupForm = popupPlaceProfile.querySelector('.popup__form');
-const nameInput = profilePopupForm.querySelector('.popup__input-text_type_name');
-const jobInput = profilePopupForm.querySelector('.popup__input-text_type_job');
+const nameInput = profilePopupForm.querySelector('#popup__input-name');
+const jobInput = profilePopupForm.querySelector('#popup__input-job');
 const popupPlaceElement = body.querySelector('.popup_place_element');
 const elementPopupForm = popupPlaceElement.querySelector('.popup__form');
 const popupPlaceImage = body.querySelector('.popup_place_image');
 const popupImage = popupPlaceImage.querySelector('.popup__image');
 const popupImageDescription = popupPlaceImage.querySelector('.popup__image-description');
-const titleInput = elementPopupForm.querySelector('.popup__input-text_type_title');
-const linkInput = popupPlaceElement.querySelector('.popup__input-text_type_link');
+const titleInput = elementPopupForm.querySelector('#popup__input-title');
+const linkInput = popupPlaceElement.querySelector('#popup__input-link');
 
-function getElement(item) {
-    const elementTemplate = document.querySelector('#element-template').content.cloneNode(true);
-    const element = elementTemplate.querySelector('.element');
-    const likeButton = element.querySelector('.element__like-button');
-    const removeButton = element.querySelector('.element__remove_button');
-    const elementImage = element.querySelector('.element__image');
+const getElement = (item) => {
+  const elementTemplate = document.querySelector('#element-template').content.cloneNode(true);
+  const element = elementTemplate.querySelector('.element');
+  const likeButton = element.querySelector('.element__like-button');
+  const removeButton = element.querySelector('.element__remove_button');
+  const elementImage = element.querySelector('.element__image');
 
-    elementImage.src = item.link;
-    elementImage.alt = item.name;
-    element.querySelector('.element__title').textContent = item.name;
+  elementImage.src = item.link;
+  elementImage.alt = item.name;
+  element.querySelector('.element__title').textContent = item.name;
 
-    likeButton.addEventListener('click', () => { toggleLike(likeButton) });
-    removeButton.addEventListener('click', () => { removeElement(element) });
-    elementImage.addEventListener('click', () => { openPopupImage(item) });
+  likeButton.addEventListener('click', () => {
+    toggleLike(likeButton)
+  });
+  removeButton.addEventListener('click', () => {
+    removeElement(element)
+  });
+  elementImage.addEventListener('click', () => {
+    openPopupImage(item)
+  });
 
-    return element;
-}
+  return element;
+};
 
-function openPopupImage(item) {
+const openPopupImage = (item) => {
   popupImage.src = item.link;
   popupImage.setAttribute('alt', item.name);
   popupImageDescription.textContent = item.name;
 
   openPopup(popupPlaceImage);
-}
+};
 
-function toggleLike(likeButton) {
+const toggleLike = (likeButton) => {
   likeButton.classList.toggle('element__like-button_active');
-}
+};
 
-function removeElement(element) {
+const removeElement = (element) => {
   element.remove();
-}
+};
 
-function openPopup(modalWindow) {
+const openPopup = (modalWindow) => {
   modalWindow.classList.add('popup_opened');
-}
+};
 
-function closePopup(modalWindow) {
+const closePopup = (modalWindow) => {
   modalWindow.classList.remove('popup_opened');
-}
+};
 
-function handleProfileFormSubmit(evt) {
+const handleProfileFormSubmit = (evt) => {
   evt.preventDefault();
 
   profileInfoTitle.textContent = nameInput.value;
   profileInfoCurenetText.textContent = jobInput.value;
 
   closePopup(popupPlaceProfile);
-}
+};
 
-function handleAddElement(evt) {
+const handleAddElement = (evt) => {
   evt.preventDefault();
 
   const element = getElement( {name: titleInput.value, link: linkInput.value} );
@@ -104,29 +110,39 @@ function handleAddElement(evt) {
   elementPopupForm.reset();
 
   closePopup(popupPlaceElement);
-}
+};
 
-profileEditButton.addEventListener('click',
-  function() {
-    nameInput.value = profileInfoTitle.textContent;
-    jobInput.value = profileInfoCurenetText.textContent;
+profileEditButton.addEventListener('click', () => {
+  nameInput.value = profileInfoTitle.textContent;
+  jobInput.value = profileInfoCurenetText.textContent;
 
-    openPopup(popupPlaceProfile);
+  openPopup(popupPlaceProfile);
 });
 
 profilePopupForm.addEventListener('submit',handleProfileFormSubmit);
 
-profileAddButton.addEventListener('click',
-  function() {
-    openPopup(popupPlaceElement);
+profileAddButton.addEventListener('click', () => {
+  openPopup(popupPlaceElement);
 });
 
 elementPopupForm.addEventListener('submit', handleAddElement);
 
 popupList.forEach((popup) => {
-    const closePopupButton = popup.querySelector('.popup__close-button');
-    closePopupButton.addEventListener('click', () => {
+  const closePopupButton = popup.querySelector('.popup__close-button');
+  closePopupButton.addEventListener('click', () => {
+    closePopup(popup);
+  });
+
+  popup.addEventListener('click', (event) => {
+    if (event.target === event.currentTarget) {
       closePopup(popup);
+    }
+  });
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      closePopup(popup);
+    }
   });
 });
 
---
+++ scripts/validate.js
@@ -0,0 +1,74 @@
+function enableValidation(options) {
+  const formList = document.querySelectorAll(options.formSelector);
+
+  formList.forEach( (form) => {
+    const inputList = [...form.querySelectorAll(options.inputSelector)];
+    const buttonElement = form.querySelector(options.submitButtonSelector);
+
+    inputList.forEach( (element) => {
+      const formError = form.querySelector(`.${element.id}-error`);
+      element.addEventListener('input', () => {
+        isValid(element, formError, options.inputErrorClass, options.errorClass)
+        toggleButtonState(inputList, buttonElement, options.inactiveButtonClass);
+      });
+    });
+  });
+};
+
+function showInputError(element, formError, inputErrorClass, errorClass) {
+    element.classList.add(inputErrorClass);
+    formError.classList.add(errorClass);
+    formError.textContent = element.validationMessage;
+};
+
+function hideInputError(element, formError, inputErrorClass, errorClass) {
+  element.classList.remove(inputErrorClass);
+  formError.classList.remove(errorClass);
+  formError.textContent = '';
+};
+
+function isValid(element, formError, inputErrorClass, errorClass) {
+  if ( !element.checkValidity() ) {
+    showInputError(element, formError, inputErrorClass, errorClass)
+  } else (
+    hideInputError(element, formError, inputErrorClass, errorClass)
+  )
+};
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+
+    return !inputElement.checkValidity();
+  });
+};
+
+function toggleButtonState(inputList, buttonElement, inactiveButtonClass) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(inactiveButtonClass);
+    buttonElement.setAttribute('disabled', '');
+  } else {
+    buttonElement.classList.remove(inactiveButtonClass);
+    buttonElement.removeAttribute('disabled');
+  }
+}
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input-text',
+  inputErrorClass: 'popup__input-text_type_error',
+  errorClass: 'popup__input-error_active',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_disabled'
+});
+
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+
+// enableValidation({
+//   formSelector: '.popup__form',
+//   inputSelector: '.popup__input',
+//   submitButtonSelector: '.popup__button',
+//   inactiveButtonClass: 'popup__button_disabled',
+//   inputErrorClass: 'popup__input_type_error',
+//   errorClass: 'popup__error_visible'
+// });
